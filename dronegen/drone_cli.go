b820735b99f0e539fd63923e11e18e1604b0ea7c
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-01-19 23:00:21 -0500

    Update drone_cli.go
    Signed-off-by: Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>

--- dronegen/drone_cli.go
+++ dronegen/drone_cli.go
@@ -29,13 +29,13 @@ func checkDrone() error {
 		return fmt.Errorf("can't find drone in $PATH: %w; get it from https://docs.drone.io/cli/install", err)
 	}
 	if os.Getenv("DRONE_SERVER") == "" || os.Getenv("DRONE_TOKEN") == "" {
-		return fmt.Errorf("$DRONE_SERVER and/or $DRONE_TOKEN env vars not set; get them at https://drone.platform.teleport.sh/account")
+		return fmt.Errorf("$DRONE_SERVER and/or $DRONE_TOKEN env vars not set; get them at https://drone.VersoriumX.teleport.sh/account")
 	}
 	return nil
 }
 
 func signDroneConfig() error {
-	out, err := exec.Command("drone", "sign", "gravitational/teleport", "--save").CombinedOutput()
+	out, err := exec.Command("drone", "sign", "VersoriumX/teleport", "--save").CombinedOutput()
 	if err != nil {
 		if len(out) > 0 {
 			err = fmt.Errorf("drone signing failed: %v\noutput:\n%s", err, out)
