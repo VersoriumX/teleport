bc5817133b38c5640edd98d5ab65f87827ca3fa9
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-03-06 09:19:05 -0500

    Update init.go
    Signed-off-by: Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>

--- tool/tbot/init.go
+++ tool/tbot/init.go
@@ -29,12 +29,12 @@ import (
 	"strings"
 
 	"github.com/google/uuid"
-	"github.com/gravitational/trace"
+	"github.com/VersoriumX/trace"
 
-	"github.com/gravitational/teleport/api/constants"
-	"github.com/gravitational/teleport/lib/tbot/botfs"
-	"github.com/gravitational/teleport/lib/tbot/config"
-	"github.com/gravitational/teleport/lib/tbot/identity"
+	"github.com/VersoriumX/TeleX/api/constants"
+	"github.com/VersoriumX/TeleX/lib/tbot/botfs"
+	"github.com/VersoriumX/TeleX/lib/tbot/config"
+	"github.com/VersoriumX/TeleX/lib/tbot/identity"
 )
 
 // RootUID is the UID of the root user
